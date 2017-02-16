b422821bbdb7a1b7c121896b48e0a0aa19bd227d
sunazerty <lamotte.alexis03@gmail.com>
2017-02-16 14:52:56 +0100

    Version 1.1

---
+++ pkmn/herbizarre.bat
@@ -0,0 +1,32 @@
+chcp 28591 > nul
+ECHO OFF
+COLOR 18
+CLS
+ECHO Herbizarre - Ivysaur
+ECHO Pokémon Graine
+ECHO Type : Plante-Poison
+ECHO Taille : 1.0 m
+ECHO Poids : 13.0 kg
+ECHO Description :
+ECHO 	Rouge et Bleu : 		Alors que son bulbe grossit, il semble perdre son habilité à se tenir sur ses pattes.
+ECHO 	Jaune : 			En emmagasinant de l'énergie, son bulbe grossit. Il en émane un arôme quand il va éclore.
+ECHO 	Or : 				La lumière du soleil le rend plus fort et vigoureux. Le bulbe sur son dos grossit en conséquence.
+ECHO 	Argent : 			Si le bourgeon sur son dos commence a sentir bon, la fleur risque d'éclore bientôt.
+ECHO 	Cristal : 			Le bulbe sur son dos grandit en absorbant les nutriments. Il dégage un doux parfum après éclosion.
+ECHO 	Rubis et Saphir : 		Un bourgeon a poussé sur le dos de ce Pokémon. Pour en supporter le poids, Herbizarre a dû se muscler les pattes.
+ECHO 					Lorsqu'il commence à se prélasser au soleil, ça signifie que son bourgeon va éclore, donnant naissance à une fleur.
+ECHO 	Emeraude :			Herbizarre a les pattes musclées afin de supporter le poids du bourgeon qu'il a sur le dos.
+ECHO 					S'il se prélasse au soleil, son bourgeon va donner naissance à une fleur.
+ECHO 	Rouge Feu : 			Il y a un bulbe sur son dos.
+ECHO						On dit que s'il absorbe assez de nutriments, ce bulbe se transforme en une jolie fleur.
+ECHO 	Vert Feuille : 			Son bulbe dorsal devient si gros qu'il ne peut plus se tenir sur ses pattes arrières.
+ECHO 	Diamant, Perle et Platine : 	Lorsque le bourgeon sur son dos éclot, il répand un doux parfum pour célébrer sa floraison.
+ECHO 	Or HeartGold : 			La lumière du soleil le rend plus fort et vigoureux. Le bulbe sur son dos grossit en conséquence.
+ECHO 	Argent SoulSilver : 		Si le bourgeon sur son dos commence à sentir bon, la fleur risque d'éclore bientôt.
+ECHO 	Noir et Blanc : 		Lorsque le bourgeon sur son dos éclot, il répand un doux parfum pour célébrer sa floraison.
+ECHO 	Noir 2 et Blanc 2 : 		Lorsque le bourgeon sur son dos éclot, il répand un doux parfum pour célébrer sa floraison.
+ECHO 	X : 				Il a un bulbe sur son dos. On dit que s'il absorbe assez de nutriments, ce bulbe se transforme en une jolie fleur.
+ECHO 	Y : 				Lorsque le bourgeon sur son dos éclot, il répand un doux parfum pour célébrer sa floraison.
+ECHO 	Rubis Oméga et Saphir Alpha :	Un bourgeon a poussé sur le dos de ce Pokémon. Pour en supporter le poids, Herbizarre a dû se muscler les pattes.
+ECHO 					Lorsqu'il commence à se prélasser au soleil, ça signifie que son bourgeon va éclore, donnant naissance à une fleur.
+..\bin\prog2.bat
